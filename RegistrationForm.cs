f824f2f53f70cc7577db904eb5aa93bf86ebc9ff
quwinner <dancatop@yandex.ru>
2018-02-10 17:07:27 +0300

    сообщения

---
+++ RegistrationForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LoginForm rf = new LoginForm();
+            rf.ShowDialog();
+        }
+
+        private void RegistrationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+
+
+
+        private void Reg_Log_Enter(object sender, EventArgs e)
+        {
+            if (loginTextBox.Text == "Введите логин")
+            {
+                loginTextBox.Text = "";
+            }
+        }
+
+        private void Reg_Log_Leave(object sender, EventArgs e)
+        {
+            if (loginTextBox.Text == "")
+            {
+                loginTextBox.Text = "Введите логин";
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ParolTextBox_Leave(object sender, EventArgs e)
+        {
+            if (ParolTextBox.Text == "")
+            {
+                ParolTextBox.Text = "Введите пароль";
+            }
+            else
+            {
+                ParolTextBox.PasswordChar = '*';
+            }
+        }
+
+        private void PotParolTextBox4_Leave(object sender, EventArgs e)
+        {
+            if (PotParolTextBox.Text == "")
+            {
+                PotParolTextBox.Text = "Повторите пароль";
+            }
+            else
+            {
+                PotParolTextBox.PasswordChar = '*';
+            }
+        }
+
+        private void PotParolTextBox4_Enter(object sender, EventArgs e)
+        {
+            if (PotParolTextBox.Text == "Повторите пароль")
+            {
+                PotParolTextBox.Text = "";
+            }
+
+        }
+
+        private void ParolTextBox_Enter(object sender, EventArgs e)
+        {
+            if(ParolTextBox.Text == "Введите пароль")
+            {
+                ParolTextBox.Text = "";
+            }
+        }
+    }
+
+       
+}
