d0aefdf28c79d43273825d0ff5620e87781a01eb
mabramovsd <beavisabra@yandex.ru>
2018-02-22 12:02:46 +0400

    Чтение переговоров by quWinner

--- WindowsFormsApplication1/Chat.cs
+++ WindowsFormsApplication1/Chat.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
-{    public struct Soobshenie
+{    
+	public struct Soobshenie
     {
         public string login;
         public string text;
     };
+
     public partial class Chat : Form
     {
         public string login;
@@ -21,8 +24,7 @@ namespace WindowsFormsApplication1
         public Chat(string _login)
         {
             InitializeComponent();
-            login = _login;
-           
+            login = _login;           
 
             saveFileDialog1.Filter = "Text files(*SaveFileDialog.txt)|*.txt|All files(*.*)|*.*";
             openFileDialog1.Filter = "Text files(*OpenFileDialog.txt)|*.txt|All files(*.*)|*.*";
@@ -41,46 +43,38 @@ namespace WindowsFormsApplication1
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            string filename = "peregovory.txt";
-            string fileText = System.IO.File.ReadAllText(filename);
-            pictureBox_Chat.Image = Image.FromFile("fon.jpg");
-            
-            textBox2.Text = fileText; 
-        /*
-            for (int n = 0; n < 11; n++)
+            FileStream file2 = new FileStream("peregovory.txt", FileMode.Open); //создаем файловый поток
+            StreamReader reader = new StreamReader(file2); // создаем «потоковый читатель» и связываем его с файловым потоком 
+
+            int i = 0;
+            while (reader.Peek() >= 0)
             {
-                DateTime thisDay = DateTime.Now;
-                textBox2.Text += thisDay.ToString("dd-MM-yyyy hh:mm:ss") + Environment.NewLine + sobch[n].login + ":" + sobch[n].text + Environment.NewLine;
+                string stroka_iz_faila = reader.ReadLine().Trim();
+                string[] podstroki = stroka_iz_faila.Split(new String[] { "$~#~@*&" }, StringSplitOptions.None);
+
+                textBox2.Text = textBox2.Text + podstroki[0] + Environment.NewLine;
+                textBox2.Text = textBox2.Text + podstroki[1] + Environment.NewLine;
+                textBox2.Text = textBox2.Text + podstroki[2] + Environment.NewLine;
+                i++;
             }
-        */
+
+            reader.Close(); //закрываем поток
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "")
-            { }
-        else
+            if (textBox1.Text != "")
             {
-            DateTime thisDay = DateTime.Now;
-            textBox2.Text += thisDay.ToString("dd-MM-yyyy hh:mm:ss") + Environment.NewLine + login + ":   " + textBox1.Text + Environment.NewLine;
-            textBox1.Text = "";
+                DateTime thisDay = DateTime.Now;
+                textBox2.Text += thisDay.ToString("dd-MM-yyyy hh:mm:ss") + Environment.NewLine + login + ":   " + textBox1.Text + Environment.NewLine;
+            
+                System.IO.File.AppendAllText("peregovory.txt", Environment.NewLine + thisDay.ToString("dd-MM-yyyy hh:mm:ss") + "$~#~@*&" + login + "$~#~@*&" + textBox1.Text);
+                textBox1.Text = "";
             }
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            DateTime thisDay = DateTime.Now;
-            string filename = "peregovory.txt";
-           // System.IO.File.WriteAllText(filename, "");
-            System.IO.File.AppendAllText(filename, Environment.NewLine + thisDay.ToString("dd-MM-yyyy hh:mm:ss") + "$~#~@*&" + login + "$~#~@*&" + textBox1.Text);
-            
-
-            
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            
+        {            
         }
     }
 }
