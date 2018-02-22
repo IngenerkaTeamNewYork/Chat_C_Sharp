d2e57928977e88afb7dd03e40d5d713941939408
mabramovsd <beavisabra@yandex.ru>
2018-02-22 11:31:16 +0400

    CHanges by Zara

--- WindowsFormsApplication1/Authorization.cs
+++ WindowsFormsApplication1/Authorization.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApplication1
     public partial class LoginForm : Form
     {
         public Polzovatel user1;
-        public static Polzovatel[] usery;
+        public static Polzovatel[] usery = new Polzovatel[500];
         public static int kolichestvo_userov;
 
         public LoginForm()
@@ -29,10 +29,7 @@ namespace WindowsFormsApplication1
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            usery = new Polzovatel[500];
-
-          
+        {          
             FileStream file2 = new FileStream("password3.txt", FileMode.Open); //создаем файловый поток
             StreamReader reader = new StreamReader(file2); // создаем «потоковый читатель» и связываем его с файловым потоком 
 
@@ -42,10 +39,12 @@ namespace WindowsFormsApplication1
                 string stroka_iz_faila = reader.ReadLine().Trim();
                 string[] podstroki = stroka_iz_faila.Split(new Char[] { ' ' });
                 usery[i].login = podstroki[0];
-                usery[i].password = podstroki[1];
+                usery[i].password = podstroki[1];  
                 i++;
             }
+
             kolichestvo_userov = i;
+            /*Console.WriteLine(reader.ReadToEnd()); //считываем все данные с потока и выводим на экран*/
             reader.Close(); //закрываем поток
 
          
@@ -53,8 +52,36 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Chat f = new Chat();
-            f.ShowDialog();
+            user1.password = PPorolTextBox.Text;
+            PPorolTextBox.PasswordChar = '*';
+
+            bool net_polzovatelya = true;
+            bool ne_pomnit_parol = true;
+            for (int i = 0; i < kolichestvo_userov; i++)
+            {
+                if (usery[i].login == user1.login)
+                {
+                    net_polzovatelya = false;
+                    if (usery[i].password == user1.password)
+                    {
+                        ne_pomnit_parol = false;
+                        MessageBox.Show("अच्छी तरह से किया, अनुवाद करें");
+
+                        Chat chatForm = new Chat(LoginTextBox.Text);
+                        chatForm.ShowDialog();
+                    }
+
+                }
+            }
+            if (net_polzovatelya)
+            {
+                MessageBox.Show("Сиди, вспоминай имя, идиот");
+            }
+            else if (ne_pomnit_parol)
+            {
+                MessageBox.Show("Сиди, вспоминай пароль, идиот");
+            }              
+
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -91,49 +118,22 @@ namespace WindowsFormsApplication1
         {
             if (PPorolTextBox.Text == "")
             {
-                PPorolTextBox.Text = "ПАроль";
+                PPorolTextBox.Text = "Пароль";
             }
             else 
-            {
-                user1.password = PPorolTextBox.Text;
-                PPorolTextBox.PasswordChar = '*';
-
-                bool net_polzovatelya = true;
-                bool ne_pomnit_parol = true;
-                for (int i = 0; i < 2; i++ )
-                {        
-                    if (usery[i].login == user1.login)
-                    {
-                        net_polzovatelya = false;
-                        if (usery[i].password == user1.password)
-                        {
-                            ne_pomnit_parol = false;
-                            MessageBox.Show("अच्छी तरह से किया, अनुवाद करें");
-                        }
-
-                    }
-                }
-                    if (net_polzovatelya)
-                    {
-                        MessageBox.Show("Сиди, вспоминай имя, идиот");
-                    }
-                    else if (ne_pomnit_parol)
-                    {
-                        MessageBox.Show("Сиди, вспоминай пароль, идиот");
-                    }
-                    
+            {      
             }
         }
 
         private void PPorolTextBox_Enter(object sender, EventArgs e)
         {
-            if (PPorolTextBox.Text == "ПАроль")
+            if (PPorolTextBox.Text == "Пароль")
             {
                 PPorolTextBox.Text = "";
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void PPorolTextBox_TextChanged(object sender, EventArgs e)
         {
 
         }
