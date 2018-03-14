b203d3dd049dbe9ad6e598f9b22a5b628df1381f
Zara28 <31924139+Zara28@users.noreply.github.com>
2018-03-14 15:09:48 +0300

    См. название ветки

--- WindowsFormsApplication1/Chat.cs
+++ WindowsFormsApplication1/Chat.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace WindowsFormsApplication1
 {    
@@ -18,15 +20,83 @@ namespace WindowsFormsApplication1
         public string text;
         public DateTime day;
     };
-   
+
+    public struct Polzovatel_view
+    {
+        public string shrift;
+        public string color;
+    };
 
     public partial class Chat : Form
     {
         public static Soobshenie[] messages = new Soobshenie[15016];
-       
+        public Polzovatel_view user2;
+        public static Polzovatel_view [] type = new Polzovatel_view [3];
+
         public string login;
         public const string rasd = "$~#~@*&";
+        public string str = " ";
+        public string str2 = " ";
 
+        public static DateTime GetNetworkTime()
+        {
+            //default Windows time server
+            const string ntpServer = "pool.ntp.org";
+
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            var ntpData = new byte[48];
+
+            //Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+            //The UDP port number assigned to NTP is 123
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            //NTP uses UDP
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Connect(ipEndPoint);
+
+                //Stops code hang if NTP is blocked
+                socket.ReceiveTimeout = 3000;
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+                socket.Close();
+            }
+
+            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            //departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
+
+            //Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+            //Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+            //Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            //**UTC** time
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime.ToLocalTime();
+        }
+
+        // stackoverflow.com/a/3294698/162671
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                        ((x & 0x0000ff00) << 8) +
+                        ((x & 0x00ff0000) >> 8) +
+                        ((x & 0xff000000) >> 24));
+        }
         public Chat(string _login)
         {
             InitializeComponent();
@@ -47,6 +117,8 @@ namespace WindowsFormsApplication1
         {
             bool sos = false;
             FileStream file2 = null;
+
+          
             try {
                 file2 = new FileStream("peregovory.txt", FileMode.Open); //создаем файловый поток
             } catch (System.IO.FileNotFoundException) {
@@ -58,6 +130,7 @@ namespace WindowsFormsApplication1
                 file2.Close();
                 file2 = new FileStream("peregovory.txt", FileMode.Open); //создаем файловый поток
                 StreamReader reader = new StreamReader(file2); // создаем «потоковый читатель» и связываем его с файловым потоком
+
                 this.Height = 651;
                 this.Width = 630;
 
@@ -116,7 +189,7 @@ namespace WindowsFormsApplication1
         {
             if (textBox1.Text.Trim() != "")
             {
-                DateTime thisDay = DateTime.Now;
+                DateTime thisDay = GetNetworkTime();
                 String dateStr = thisDay.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz");
                 String dateStrfors = thisDay.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz");
 
@@ -160,11 +233,25 @@ namespace WindowsFormsApplication1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            fontDialog1.ShowColor = true;            
-            fontDialog1.Font = textBox1.Font;
-            fontDialog1.Color = textBox1.ForeColor;
+            FileStream config = new FileStream("config.txt", FileMode.Open); //создаем файловый поток
+            StreamReader reader2 = new StreamReader(config); // создаем «потоковый читатель» и связываем его с файловым потоком 
 
-            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+
+            while (reader2.Peek() >= 0)
+            {
+                string stroka = reader2.ReadLine().Trim();
+                string[] view = stroka.Split(new Char[] { '$' });
+                str = view[0];
+            }
+            config.Close();
+
+            Font f1 = new Font(str, 10);
+
+            fontDialog1.ShowColor = true;
+            fontDialog1.Font = f1;// textBox1.Font;
+            fontDialog1.Color = textBox1.ForeColor;
+            fontDialog1.ShowDialog();
+            //if (fontDialog1.ShowDialog() != DialogResult.Cancel)
             {
                 textBox1.Font = fontDialog1.Font;
                 textBox2.Font = fontDialog1.Font;
@@ -180,14 +267,16 @@ namespace WindowsFormsApplication1
 
                 button1.Font = fontDialog1.Font;
                 button2.Font = fontDialog1.Font;
-                button3.Font = fontDialog1.Font; 
+                button3.Font = fontDialog1.Font;
+
+                File.WriteAllText("config.txt", textBox1.Font.FontFamily.Name.ToString() + "$" + textBox1.ForeColor);
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             File.WriteAllText("AllMessages.txt", string.Empty);
-            Process myProcess = Process.Start("get.exe", "peregovory.txt peregovory.txt");
+            Process myProcess = Process.Start("cmd", "/C start /B get.exe peregovory.txt peregovory.txt");
             do
             {
                 if (!myProcess.HasExited)
@@ -214,7 +303,7 @@ namespace WindowsFormsApplication1
             textBox2.Clear();
             Form2_Load(sender, e);
 
-            Process.Start("put.exe", "peregovory.txt peregovory.txt");
+            Process.Start("cmd", "/C start /B get.exe peregovory.txt peregovory.txt");
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -224,6 +313,9 @@ namespace WindowsFormsApplication1
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            
         }
+
+       
     }
 }
