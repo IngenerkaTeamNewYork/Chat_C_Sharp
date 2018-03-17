4ea43c0c2d54f33e9734ffc8e79a2e075b25840c
Zara28 <31924139+Zara28@users.noreply.github.com>
2018-03-17 17:22:28 +0300

    Распихать по файлам

---
+++ WindowsFormsApplication1/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public static class Const
+    {
+        public const string rasd = "$~#~@*&";
+        public const string adressForGet = "/C start /B get.exe peregovory.txt peregovory.txt";
+        public const string adressForPut = "/C start /B put.exe peregovory.txt peregovory.txt";
+        public const string enter = "%%%%";
+        public const string ntpServer = "pool.ntp.org";
+        public const string separator = "$";
+        public const string peregovory = "peregovory.txt";
+        public const string NewMessages = "NewMessages.txt";
+        public const string AllMessages = "AllMessages.txt";
+    }
+}
