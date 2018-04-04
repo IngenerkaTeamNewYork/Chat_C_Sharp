82c2f214ac4f90829edf719ffee74dcd4b36fa64
NeverMine17 <dannevergame@gmail.com>
2018-04-04 20:31:35 +0400

    Revert "comboBox для добавления собеседников."

--- WindowsFormsApplication1/Chat.cs
+++ WindowsFormsApplication1/Chat.cs
@@ -27,24 +27,19 @@ namespace WindowsFormsApplication1
 
         public string login;
         public const string rasd = "$~#~@*&";
-        public string subchat = "peregovory";
+		public string subchat = "peregovory";
 
-        public Chat(string _login, String _subchat = "peregovory")
+        public Chat(string _login, String _subchat="peregovory")
         {
             InitializeComponent();
             login = _login;
             subchat = _subchat;
 
-            for (int i = 0; i < LoginForm.kolichestvo_userov; i++ )
-            {
-                comboBox1.Items.Add(LoginForm.usery[i].login);
-            }
-
             saveFileDialog1.Filter = "Text files(*SaveFileDialog.txt)|*.txt|All files(*.*)|*.*";
             openFileDialog1.Filter = "Text files(*OpenFileDialog.txt)|*.txt|All files(*.*)|*.*";
         }
 
-
+        
 
 
         private void badWords()
@@ -76,8 +71,7 @@ namespace WindowsFormsApplication1
                 ChatProcessing.SubChat dsad = new ChatProcessing.SubChat(subchat);
                 messages = dsad.LoadChat(login);
                 textBox2.Text = dsad.PrintChat(login, messages);
-            }
-            catch (UnauthorizedAccessException err)
+            } catch (UnauthorizedAccessException err)
             {
                 MessageBox.Show(err.ToString());
                 this.Close();
@@ -112,7 +106,7 @@ namespace WindowsFormsApplication1
 
             FileStream file2 = new FileStream(subchat + ".txt", FileMode.Open); //создаем файловый поток
             StreamReader reader = new StreamReader(file2); // создаем «потоковый читатель» и связываем его с файловым потоком
-
+            
             i = 0;
 
             while (reader.Peek() >= 0)
@@ -174,15 +168,15 @@ namespace WindowsFormsApplication1
 
             if (view.Length > 4)
             {
-                Font fontFormFile = new Font(view[0], (float)Convert.ToDouble(view[1]));
-
+                Font fontFormFile = new Font (view[0], (float)Convert.ToDouble(view[1]));
+                
                 if (view[2] == "True")
                 {
-                    fontFormFile = new Font(view[0], (float)Convert.ToDouble(view[1]), FontStyle.Italic);
+                    fontFormFile = new Font (view[0], (float)Convert.ToDouble(view[1]), FontStyle.Italic);
                 }
                 else if (view[3] == "True")
                 {
-                    fontFormFile = new Font(view[0], (float)Convert.ToDouble(view[1]), FontStyle.Bold);
+                    fontFormFile = new Font (view[0], (float)Convert.ToDouble(view[1]), FontStyle.Bold);
                 }
 
                 textBox1.Font = fontFormFile;
@@ -208,23 +202,23 @@ namespace WindowsFormsApplication1
             fontDialog1.Color = textBox1.ForeColor;
 
             if (fontDialog1.ShowDialog() != DialogResult.Cancel)
-            {
+            {                
                 ColorDialog MyDialog = new ColorDialog();
                 MyDialog.AllowFullOpen = false;
                 MyDialog.ShowHelp = true;
                 MyDialog.Color = textBox1.ForeColor;
-
+                
                 if (MyDialog.ShowDialog() == DialogResult.OK)
                 {
                     textBox1.ForeColor = MyDialog.Color;
                 }
-
+                
                 textBox1.Font = fontDialog1.Font;
                 textBox2.Font = fontDialog1.Font;
                 button1.Font = fontDialog1.Font;
                 button2.Font = fontDialog1.Font;
                 button3.Font = fontDialog1.Font;
-
+                
                 textBox1.ForeColor = fontDialog1.Color;
                 textBox2.ForeColor = fontDialog1.Color;
                 button1.ForeColor = fontDialog1.Color;
@@ -232,18 +226,18 @@ namespace WindowsFormsApplication1
                 button3.ForeColor = fontDialog1.Color;
 
                 File.WriteAllText("config.txt", textBox1.Font.FontFamily.Name.ToString() +
-                    "$" + textBox1.Font.Size.ToString() +
+                    "$" + textBox1.Font.Size.ToString() + 
                     "$" + textBox1.Font.Italic.ToString() +
-                    "$" + textBox1.Font.Bold.ToString() +
+                    "$" + textBox1.Font.Bold.ToString() + 
                     "$" + textBox1.ForeColor.ToArgb());
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
-        {
+        {        
             File.WriteAllText("AllMessages.txt", string.Empty);
             GetPut.Get(subchat + ".txt");
-
+                
 
             FileStream file2 = new FileStream("NewMessages.txt", FileMode.Open);
             StreamReader reader = new StreamReader(file2); // создаем «потоковый читатель» и связываем его с файловым потоком
@@ -278,8 +272,8 @@ namespace WindowsFormsApplication1
         {
             SwearWords.Add(textBox2.SelectedText);
         }
-
-        private void RemoveBadWordToolStripMenuItem_Click(object sender, EventArgs e)
+		
+		private void RemoveBadWordToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show(textBox1.SelectedText);
             SwearWords.Remove(textBox1.SelectedText);
@@ -316,11 +310,7 @@ namespace WindowsFormsApplication1
 
         private void button5_Click(object sender, EventArgs e)
         {
-            File.AppendAllLines(subchat + "-users.txt", new String[] { comboBox1.Text });
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
+            File.AppendAllLines(subchat + "-users.txt", new String[] { textBox4.Text });
         }
     }
 }
