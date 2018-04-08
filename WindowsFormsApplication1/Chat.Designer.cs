4c59424dfe9240cb258b594cfb399abf3d9f6004
Mikhail Abramov <beavisabra@yandex.ru>
2018-04-08 12:17:25 +0400

    Update Chat.Designer.cs

--- WindowsFormsApplication1/Chat.Designer.cs
+++ WindowsFormsApplication1/Chat.Designer.cs
@@ -232,7 +232,7 @@ namespace WindowsFormsApplication1
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(122, 23);
             this.button6.TabIndex = 15;
-            this.button6.Text = "button6";
+            this.button6.Text = "у меня есть ягуар";
             this.button6.UseVisualStyleBackColor = true;
             //
             // Chat
